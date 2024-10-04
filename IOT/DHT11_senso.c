f10367f689e7048d51dfa90524e9e4c7ef786c61
root <root@DESKTOP-UBGV9FD.localdomain>
2024-10-04 18:28:32 +0100

    About DHT11 sensor

---
+++ IOT/DHT11_senso.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+// This library can only be used with a raspberry pi board.
+#include <wiringPi.h>
+//Same with this one
+#include <wiringPiI2C.h>
+#include <mosquitto.h>
+
+#define DHT11_PIN 7
+
+int main() {
+  wiringPiSetupGpio();
+
+  int dht11_fd = wiringPiI2CSetup(0x40);
+
+  struct mosquitto *mosq;
+  mosquitto_lib_init();
+  mosq = mosquitto_new("temperature_sensor", true, NULL);
+  mosquitto_connect(mosq, "broker.hivemq.com", 1883, 60);
+
+  while (1) {
+    int data[5] = {0};
+    if (readDHT11(dht11_fd, data) == 0) {
+      float temperature = data[2] + data[3] / 10.0;
+      printf("Temperature: %.2f degrees Celsius\n", temperature);
+
+      char temperature_str[10];
+      sprintf(temperature_str, "%.2f", temperature);
+      mosquitto_publish(mosq, NULL, "temperature", strlen(temperature_str), temperature_str, 0);
+    }
+
+    delay(1000);
+  }
+
+  return 0;
+}
